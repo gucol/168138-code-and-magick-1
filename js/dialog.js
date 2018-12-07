f0699272f59e5751cf0e538678043b01d05c6a6b
gucol <ezhi@meta.ua>
2018-12-07 11:15:39 +0300

    Исправляет ошибки и инкапсулирует модуль

--- js/dialog.js
+++ js/dialog.js
@@ -10,65 +10,20 @@
 
 'use strict';
 /*
-Сначала найдём тот элемент, за который будем тащить наш диалог setup-user-pic, 
+Сначала найдём тот элемент, за который будем тащить наш диалог setup-user-pic,
 и обработаем событие начала перетаскивания нашего диалога mousedown.
 */
-var setup = document.querySelector('.setup');
-var dialogHandle = setup.querySelector('.setup-user-pic');
-var dragged;
-var startCoords;
+( function () {
+  var setup = document.querySelector('.setup');
+  var dialogHandler = setup.querySelector('.upload');
+  var dragged;
+  var startCoords;
 
-// Обработчик события перемещения курсора
-var onMouseMove = function (moveEvt) {
-  // Удаление событи по умолчанию, см. MDN: https://developer.mozilla.org/ru/docs/Web/API/Event/preventDefault
-  moveEvt.preventDefault();
-
-  // Фиксируем, что произошло перемещение:
-  dragged = true;
-
-  // Записываем вносим изменения, фиксируем перемещение курсора:
-  var shift = {
-    x: startCoords.x - moveEvt.clientX,
-    y: startCoords.y - moveEvt.clientY
-  };
-
-  // Обновляем стартовые данные:
-  startCoords = {
-    x: moveEvt.clientX,
-    y: moveEvt.clientY
-  };
-
-  // Смещаем перетаскиваемый объект за курсором:
-  setup.style.top = (setup.offsetTop - shift.y) + 'px';
-  setup.style.left = (setup.offsetLeft - shift.x) + 'px';
-};
-
-// Как только мышку отпустили, мы перестаём слушать события движения мыши:
-var onMouseUp = function (upEvt) {
-  upEvt.preventDefault();
-
-  document.removeEventListener('mousemove', onMouseMove);
-  document.removeEventListener('mouseup', onMouseUp);
-
-  /*
-  Выбор аватара — это нажатие без перемещения, а если мы нажали и начали двигать курсор, 
-  то действие выбора файла надо отменить: с dialogHandler снимаем слушание клика, приводящего к загрузке аватарки.
-  */
-  if (dragged) {
-    var onClickPreventDefault = function (evt) {
-      evt.preventDefault();
-      dialogHandler.removeEventListener('click', onClickPreventDefault)
-    };
-    dialogHandler.addEventListener('click', onClickPreventDefault);
-  };
-};
-
-
-dialogHandle.addEventListener('mousedown', function (evt) {
+dialogHandler.addEventListener('mousedown', function (evt) {
   // Сброс событий по умолчанию:
   evt.preventDefault();
 
-  //Запомним координаты точки, с которой мы начали перемещать диалог:
+  // Запомним координаты точки, с которой мы начали перемещать диалог:
   startCoords = {
     x: evt.clientX,
     y: evt.clientY
@@ -77,6 +32,53 @@ dialogHandle.addEventListener('mousedown', function (evt) {
   // Было ли перемещение объекта?
   dragged = false;
 
+  // Обработчик события перемещения курсора
+  var onMouseMove = function (moveEvt) {
+    // Удаление событи по умолчанию, см. MDN: https://developer.mozilla.org/ru/docs/Web/API/Event/preventDefault
+    moveEvt.preventDefault();
+
+    // Фиксируем, что произошло перемещение:
+    dragged = true;
+
+    // Записываем вносим изменения, фиксируем перемещение курсора:
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    // Обновляем стартовые данные:
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    // Смещаем перетаскиваемый объект за курсором:
+    setup.style.top = (setup.offsetTop - shift.y) + 'px';
+    setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+  };
+
+  // Как только мышку отпустили, мы перестаём слушать события движения мыши:
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+
+    /*
+    Выбор аватара — это нажатие без перемещения, а если мы нажали и начали двигать курсор,
+    то действие выбора файла надо отменить: с dialogHandler снимаем слушание клика, приводящего к загрузке аватарки.
+    */
+    if (dragged) {
+      var onClickPreventDefault = function (evt) {
+        evt.preventDefault();
+        dialogHandler.removeEventListener('click', onClickPreventDefault);
+      };
+      dialogHandler.addEventListener('click', onClickPreventDefault);
+    }
+  };
+
   document.addEventListener('mousemove', onMouseMove);
   document.addEventListener('mouseup', onMouseUp);
 });
+})()
+
